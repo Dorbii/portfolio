a28bcec403fa897710f66b3461c1836618c1e432
Dorbii <stevemdoris@gmail.com>
2025-02-27 18:41:01 -0600

    fixed header button placement

--- src/Components/Desktop/AppWindow/Header.jsx
+++ src/Components/Desktop/AppWindow/Header.jsx
@@ -1,10 +1,27 @@
-import React from "react";
-import "./css/header.css";
-function Header({ onClose }) {
+import React from 'react';
+import './css/header.css';
+
+function Header({ onClose, onMaximize, onMinimize }) {
     return (
         <div className="header-container">
-            <button className="close-btn" onClick={onClose}></button>
-        </div>
+            <div className="btn-tile">
+                <div className='btn-container'>
+                    <button className="min-btn" onClick={onMinimize}></button>
+                </div>
+            </div>
+            <div className="btn-tile">
+                <div className='btn-container'>
+                    <button className="max-btn" onClick={onMaximize}></button>
+                </div>
+            </div>
+            <div className="close-btn-tile">
+                <div className='btn-container'>
+                    <button className="close-btn" onClick={onClose}></button>
+                </div>
+
+            </div>
+        </div >
     );
 }
+
 export default Header;
