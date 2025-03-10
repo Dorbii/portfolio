9659ed8eba751379a0b507abbce8a42574c4af76
Dorbii <stevemdoris@gmail.com>
2025-03-10 13:45:40 -0500

    added start icon

--- src/Components/Desktop/Taskbar/Taskbar.jsx
+++ src/Components/Desktop/Taskbar/Taskbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import './taskbar.css';
+import winIcon from '../../../assets/Desktop/taskbar/windows_icon.png';
+import { Button } from '@mui/material';
 export default function Taskbar() {
     return (
         <>
             <footer className="taskbar">
+                <div class="taskbar-container">
+                    <div class="start-icon-container">
+                        <div class="start-icon"></div>
+                    </div>
+                    <div class="app-icons-container">
+                    </div>
+                </div>
             </footer>
         </>
     );
